ecc360b5b1b5c0e87abecfbd33fb4fc00e9db759
Michael Aherne <michael.aherne@strath.ac.uk>
2021-03-22 16:52:44 +0000

    Fix method signature.

--- vars/runPluginCi.groovy
+++ vars/runPluginCi.groovy
@@ -1,3 +1,3 @@
-def call(string phpVersion = '7.2', string db = 'mysql', Map commands = [:]) {
+def call(String phpVersion = '7.2', String db = 'mysql', Map commands = [:]) {
     echo "PHP: ${phpVersion}"
 }
