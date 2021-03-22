35fdb7e22fb04f063c7cacc688cd7e94ed9b6f97
Michael Aherne <michael.aherne@strath.ac.uk>
2021-03-22 17:13:55 +0000

    Test params.

--- vars/runPluginCi.groovy
+++ vars/runPluginCi.groovy
@@ -2,8 +2,10 @@ def call(Map pipelineParams) {
 
     def php = pipelineParams.php ?: '7.2'
     def db = pipelineParams.db ?: 'mysql'
+    def commands = pipelineParams.commands ?: [:]
 
     echo "PHP: ${php}"
     echo "Database: ${db}"
+    echo "Commands: ${commands}"
 
 }
