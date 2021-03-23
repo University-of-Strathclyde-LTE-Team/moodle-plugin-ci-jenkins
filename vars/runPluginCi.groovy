90344e504962503511022f4cd4059b97b5296700
Michael Aherne <michael.aherne@strath.ac.uk>
2021-03-23 10:42:42 +0000

    Some additions.

--- vars/runPluginCi.groovy
+++ vars/runPluginCi.groovy
@@ -1,28 +1,44 @@
-def call(Map pipelineParams) {
+def call(Map pipelineParams, Closure body) {
 
     def php = pipelineParams.php ?: '7.2'
     def db = pipelineParams.db ?: 'mysql'
-    def commands = pipelineParams.commands ?: [:]
+    def install = pipelineParams.install
+
+    if (install == null) {
+        install = true
+    }
 
     echo "PHP: ${php}"
     echo "Database: ${db}"
-    echo "Commands: ${commands}"
 
     def dockerFileContents = libraryResource 'uk/ac/strath/myplace/Dockerfile'
     def dockerFile = "${WORKSPACE}/${BUILD_TAG}.Dockerfile";
+    sh 'env > ${WORKSPACE}/${BUILD_TAG}.env'
+
     writeFile(file: dockerFile, text: dockerFileContents)
 
     def image = docker.build("${BUILD_TAG}", "-f ${dockerFile} .")
 
     image.inside() {
-        sh '''
-            sudo service mysql start
 
-            mkdir ${BUILD_NUMBER} && cd ${BUILD_NUMBER}
+        withEnv(["npm_config_cache=${WORKSPACE}/.npm", "COMPOSER_CACHE_DIR=${WORKSPACE}/composer-cache"]) {
 
-            composer create-project -n --no-dev --prefer-dist moodlehq/moodle-plugin-ci ci ^3
-            PATH="$PWD/ci/bin:$PATH"
-            '''
+            sh '''
+                sudo service mysql start
+
+                composer create-project -n --no-dev --prefer-dist moodlehq/moodle-plugin-ci ci ^3
+                PATH="$PWD/ci/bin:$PATH"
+                '''
+
+            if (install) {
+                sh 'moodle-plugin-ci install --db-user jenkins --db-pass jenkins \
+                    --plugin ${WORKSPACE}/plugin'
+            }
+
+            body()
+
+        }
+
     }
 
     new File(dockerFile).delete()
