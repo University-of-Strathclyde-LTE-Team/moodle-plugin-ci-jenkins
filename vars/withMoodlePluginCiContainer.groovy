351857651b219d289150d1ef88128d5e39e46671
Michael Aherne <michael.aherne@strath.ac.uk>
2024-02-27 12:36:05 +0000

    Initialise nvm.

--- vars/withMoodlePluginCiContainer.groovy
+++ vars/withMoodlePluginCiContainer.groovy
@@ -90,7 +90,7 @@ private def runContainers(Map pipelineParams = [:], Closure body) {
     // Nasty hack to get around the fact that we can't use withEnv to change the PATH on a container
     // (or any other method as far as I can see)
     // https://issues.jenkins.io/browse/JENKINS-49076
-    def originalDockerPath = "/var/lib/nvm/versions/node/v16.19.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+    def originalDockerPath = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     def pathOnDocker = "${WORKSPACE}/ci/bin:${originalDockerPath}"
 
     image.inside("-e PATH=${pathOnDocker} --network ${buildTag} --network-alias=moodle") {
@@ -118,6 +118,7 @@ private def runContainers(Map pipelineParams = [:], Closure body) {
         }
 
         withEnv(installEnv) {
+            sh ". $NVM_DIR/nvm.sh"
             // Install plugin ci.
             sh "composer create-project -n --no-dev --prefer-dist moodlehq/moodle-plugin-ci ci ^${ciVersion}"
         }
